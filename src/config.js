af3ecbfcc9e204b788e3aa066ce81cdcb02e0e80
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-09 16:33:18 +0500

    cargotekeli client

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://cargotekeli.kz:3001',
+    
+    nameCargo: 'Cargo Tekeli'
+  };
+  
+  
+export default config;
+  
